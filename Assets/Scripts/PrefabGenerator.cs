57aba8dc445c7bf18141c86f44bbd7d6d563f398
Deftoner555 <jmoya555@gmail.com>
2023-04-18 23:45:25 -0700

    set up random instantiate script

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -6,17 +6,19 @@ public class PrefabGenerator : MonoBehaviour
 {
 
     public GameObject prefab;
+    public Vector3 spawnValues;
+
+    void Start()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1f, Random.Range(-spawnValues.z, spawnValues.z));
+            GameObject GO = Instantiate<GameObject>(prefab, spawnposition, Quaternion.identity);
+        }
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Instantiate(prefab);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(prefab, transform.position, transform.rotation);
-        }
+        
     }
 }
