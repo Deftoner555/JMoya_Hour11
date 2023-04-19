9afc6ab1bad4eda9673852bd8da3864c0dd47012
Deftoner555 <jmoya555@gmail.com>
2023-04-19 00:32:34 -0700

    custom street light, finished script

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -7,18 +7,27 @@ public class PrefabGenerator : MonoBehaviour
 
     public GameObject prefab;
     public Vector3 spawnValues;
+    public float spawnInterval;
 
     void Start()
     {
-        for (int i = 1; i <= 5; i++)
+        for (int i = 1; i <= 10; i++)
         {
-            Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1f, Random.Range(-spawnValues.z, spawnValues.z));
+            Vector3 spawnposition = new Vector3(i * spawnInterval, 1f, Random.Range(-spawnValues.z, spawnValues.z));
             GameObject GO = Instantiate<GameObject>(prefab, spawnposition, Quaternion.identity);
         }
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(prefab);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
     }
 }
